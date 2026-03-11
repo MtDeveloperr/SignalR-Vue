0e9a031c396d79ad5bd6a24eecd9c86491f21e75
MT <mtemirofficial@gmail.com>
2026-03-11 23:59:08 +0300

    fix

--- src/components/SignalRComp.vue
+++ src/components/SignalRComp.vue
@@ -1,38 +1,52 @@
 <template>
-    <div>
-      <h1>Chat</h1>
-      <input v-model="message" placeholder="Type a message" />
-      <button @click="sendMessage">Send</button>
-    </div>
-    <h3 v-show="messages.length > 0 ">Income Messages</h3>
+  <div>
+    <h1>Chat</h1>
+    <input v-model="message" placeholder="Type a message" />
+    <button @click="sendMessage">Send</button>
+
+    <h3 v-show="messages.length > 0">Incoming Messages</h3>
     <div v-for="(msg, index) in messages" :key="index">
-        {{ msg }}
-      </div>
-  </template>
-  
-  <script>
-  import { on, send, startConnection } from "../signalrService";
-  
-  export default {
-    data() {
-      return {
-        messages: [],
-        message: "",
-      };
-    },
-    async created() {
-      await startConnection();
-  
-      on(( message) => {
-        this.messages.push(message );
-      });
-    },
-    methods: {
-      sendMessage() {
-        send(this.message);
-        this.message = "";
-        console.log(this.messages);
-      },
-    },
-  };
-  </script>
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { onReceiveMessage, offReceiveMessage, send, startConnection } from "../signalrService";
+
+export default {
+  data() {
+    return {
+      messages: [],
+      message: "",
+      receiveHandler: null
+    };
+  },
+  async mounted() {
+    await startConnection();
+
+    this.receiveHandler = (message) => {
+      this.messages.push(message);
+    };
+
+    onReceiveMessage(this.receiveHandler);
+  },
+  beforeUnmount() {
+    if (this.receiveHandler) {
+      offReceiveMessage(this.receiveHandler);
+    }
+  },
+  methods: {
+    async sendMessage() {
+  if (!this.message?.trim()) return;
+
+  try {
+    await send(this.message);
+    this.message = "";
+  } catch (err) {
+    console.error("Mesaj gönderilemedi:", err);
+  }
+}
+  }
+};
+</script>
--- src/signalrService.js
+++ src/signalrService.js
@@ -1,25 +1,35 @@
 import * as signalR from "@microsoft/signalr";
 
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://localhost:7219/notifications-hub") // Replace with your hub URL
-    .withAutomaticReconnect()
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
+  .withUrl("https://localhost:7219/notifications-hub")
+  .withAutomaticReconnect()
+  .configureLogging(signalR.LogLevel.Information)
+  .build();
 
 export async function startConnection() {
-    try {
-        await connection.start();
-        console.log("SignalR Connected.");
-    } catch (err) {
-        console.error("Error while starting connection: ", err);
-        setTimeout(startConnection, 5000); // Retry connection
-    }
+  if (connection.state === signalR.HubConnectionState.Connected) return;
+
+  try {
+    await connection.start();
+    console.log("SignalR Connected.");
+  } catch (err) {
+    console.error("Error while starting connection: ", err);
+    setTimeout(startConnection, 5000);
+  }
 }
 
-export function on(callback) {
-    connection.on("ReceiveMessage", callback);
+export function onReceiveMessage(callback) {
+  connection.on("ReceiveMessage", callback);
 }
 
-export function send(data) {
-    connection.invoke("SendMessage", data).catch((err) => console.error(err));
+export function offReceiveMessage(callback) {
+  connection.off("ReceiveMessage", callback);
+}
+
+export async function send(data) {
+  try {
+    await connection.invoke("SendMessage", data);
+  } catch (err) {
+    console.error(err);
+  }
 }
